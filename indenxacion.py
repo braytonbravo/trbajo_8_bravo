f0371ab51e992bf80fd91e092da4a6270f92f693
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-12-13 02:22:47 -0500

    se desarrollo los ejercicicos de indexacion

---
+++ indenxacion.py
@@ -0,0 +1,92 @@
+# OPERACION-INDEXACION
+# EJERCICIO 1
+#              10        20
+#    01234567890123456789012345
+str="EL BALON ES MI MEJOR AMIGO"
+print(str[5] + str[16] + str[-1] + str[-19]) # Devuelve la palabra LEON
+
+# EJERCICIO 2
+#              10        20
+#    01234567890123456789012345
+str="EL BALON ES MI MEJOR AMIGO"
+print(str[0] + str[-21] + str[1] + str[21] ) # Devuelve la palabra ELLA
+
+# EJERCICIO 3
+#              10        20
+#    01234567890123456789012345
+str="EL BALON ES MI MEJOR AMIGO"
+print(str[15] + str[-3] + str[-25]+ str[4] + str[24] + str[-7]+ str[6]) # Devuelve la palabra MILAGRO
+
+# EJERCICIO 4
+#              10        20
+#    01234567890123456789012345
+str="EL BALON ES MI MEJOR AMIGO"
+print(str[3] + str[-7] + str[4] + str[-16] + str[23] + str[5]) # Devuelve la palabra Brasil
+
+# EJERCICIO 5
+#              10        20
+#    01234567890123456789012345
+str="EL BALON ES MI MEJOR AMIGO"
+print(str[-2] + str[18] + str[5]) # Devuelve la palabra GOL
+
+# EJERCICIO 6
+#              10        20        30        40        50       60        70        80
+#    01234567890123456789012345678901234567890123456789023456789012345678901234567890
+msg="MI MADRE ERA TRISTE Y MI PADRE ERA CALLADO Y LA ALEGRIA NADIE ME LA SUPO ENSEÑAR"
+print(msg[14] + msg[50] + msg[23] + msg[-1]) # Devuelve la palabra REIR
+
+# EJERCICIO 7
+#              10        20        30        40        50       60        70        80
+#    01234567890123456789012345678901234567890123456789023456789012345678901234567890
+msg="MI MADRE ERA TRISTE Y MI PADRE ERA CALLADO Y LA ALEGRIA NADIE ME LA SUPO ENSEÑAR"
+print(msg[27] + msg[41] + msg[35] + msg[13] + msg[-9] + msg[-1]) # Devueve la palabra DOCTOR
+
+# EJERCICIO 8
+#              10        20        30        40        50       60        70        80
+#    01234567890123456789012345678901234567890123456789023456789012345678901234567890
+msg="MI MADRE ERA TRISTE Y MI PADRE ERA CALLADO Y LA ALEGRIA NADIE ME LA SUPO ENSEÑAR"
+print(msg[25] + msg[11] +  msg[-6] + msg[18] + msg[13] + msg[-9] + msg[74] ) # Devueve la palabra DOCTOR
+
+# EJERCICIO 9
+#              10        20        30        40        50       60        70        80
+#    01234567890123456789012345678901234567890123456789023456789012345678901234567890
+msg="MI MADRE ERA TRISTE Y MI PADRE ERA CALLADO Y LA ALEGRIA NADIE ME LA SUPO ENSEÑAR"
+print(msg[11] + msg[3] + msg[71] + msg[32]) # Devueve la palabra AMOR
+
+# EJERCICIO 10
+#              10        20        30        40        50       60        70        80
+#    01234567890123456789012345678901234567890123456789023456789012345678901234567890
+msg="MI MADRE ERA TRISTE Y MI PADRE ERA CALLADO Y LA ALEGRIA NADIE ME LA SUPO ENSEÑAR"
+print(msg[7]+msg[37]+msg[29]+msg[35]+msg[13]+msg[28]+msg[-9]+msg[56]+msg[15]+msg[35]+msg[-2]) # Devueve la palabra ELECTRONICA
+
+# EJERCICIO 11
+#              10        20        30        40        50       60        70        80
+#    01234567890123456789012345678901234567890123456789023456789012345678901234567890
+msg="MI MADRE ERA TRISTE Y MI PADRE ERA CALLADO Y LA ALEGRIA NADIE ME LA SUPO ENSEÑAR"
+print(msg[41] + msg[6] + msg[27] + msg[31] + msg[56]) # Devueve la palabra ORDEN
+
+# EJERCICIO 12
+#              10        20        30        40        50       60        70        80
+#    01234567890123456789012345678901234567890123456789023456789012345678901234567890
+msg="MI MADRE ERA TRISTE Y MI PADRE ERA CALLADO Y LA ALEGRIA NADIE ME LA SUPO ENSEÑAR"
+print(msg[37] + msg[38] + msg[48] + msg[22] + msg[36] + msg[10]) # Devueve la palabra LLAMAR
+
+# EJERCICIO 13
+#              10        20        30        40        50       60        70        80
+#    01234567890123456789012345678901234567890123456789023456789012345678901234567890
+msg="MI MADRE ERA TRISTE Y MI PADRE ERA CALLADO Y LA ALEGRIA NADIE ME LA SUPO ENSEÑAR"
+print(msg[27] + msg[41] + msg[10] + msg[0] + msg[1] + msg[6]) # Devueve la palabra DORMIR
+
+# EJERCICIO 14
+#              10        20        30        40        50       60        70        80
+#    01234567890123456789012345678901234567890123456789023456789012345678901234567890
+msg="MI MADRE ERA TRISTE Y MI PADRE ERA CALLADO Y LA ALEGRIA NADIE ME LA SUPO ENSEÑAR"
+print(msg[53] + msg[32] + msg[4]) # Devueve la palabra IRA
+
+# EJERCICIO 15
+#              10        20        30        40        50       60        70        80
+#    01234567890123456789012345678901234567890123456789023456789012345678901234567890
+msg="MI MADRE ERA TRISTE Y MI PADRE ERA CALLADO Y LA ALEGRIA NADIE ME LA SUPO ENSEÑAR"
+print(msg[7]+msg[37]+msg[38]+msg[-2]) # Devueve la palabra ELLA
+
+
