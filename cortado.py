29211a127d01258b2ed3a421134cbc52d6509178
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-12-13 02:22:23 -0500

    se desarrollo los ejercicicos de cortado

---
+++ cortado.py
@@ -0,0 +1,78 @@
+# EJERCICIO 1
+#              10        20        30        40
+#    012345678901234567890123456789012345678901234
+msg="HAY GOLPES EN LA VIDA TAN FUERTES YO NO LO SE"
+
+# Mostrar slices
+print(msg[26:33])   # Devuelve FUERTES
+
+# EJERCICIO 2
+msg="HAY GOLPES EN LA VIDA TAN FUERTES YO NO LO SE"
+# Mostrar slices
+print(msg[-43:-46:-1])   # Devuelve YAH
+
+# EJERCICIO 3
+msg="HAY GOLPES EN LA VIDA TAN FUERTES YO NO LO SE"
+# Mostrar slices
+print(msg[4:10])   # Devuelve GOLPES
+
+# EJERCICIO 4
+msg="HAY GOLPES EN LA VIDA TAN FUERTES YO NO LO SE"
+# Mostrar slices
+print(msg[34:])   # Devuelve YO NO LO SE
+
+# EJERCICIO 5
+msg="HAY GOLPES EN LA VIDA TAN FUERTES YO NO LO SE"
+# Mostrar slices
+print(msg[22:32])   # Devuelve TAN FUERTE
+# EJERCICIO 6
+msg="HAY GOLPES EN LA VIDA TAN FUERTES YO NO LO SE"
+# Mostrar slices
+print(msg[0:4] + "" + msg[26:33] + " " + msg[4:10])  # Devuelve HAY FUERTES GOLPES
+
+# EJERCICIO 7
+#    012345678901234567890123456789
+cad="IM OSRUC FAVORITO PROGRAMACION"
+# Mostrar slices
+print(cad[-23:-28:-1])  # Delvuele CURSO
+
+# EJERCICIO 8
+cad="IM OSRUC FAVORITO PROGRAMACION"
+# Mostrar slices
+print(cad[-29:-31:-1] + " " + cad[-4:-8:-1]) # Delvuele MI CAMA
+
+# EJERCICIO 9
+cad="IM OSRUC FAVORITO PROGRAMACION"
+# Mostrar slices
+print(cad[18:])  # Delvuele PROGRAMACION
+
+# EJERCICIO 10
+cad="IM OSRUC FAVORITO PROGRAMACION"
+# Mostrar slices
+print(cad[9:17])  # Delvuele FAVORITO
+
+# EJERCICIO 11
+#   01234567890123456789012345678
+mg="RUMBO AL LICENCIAMIENTO UNPRG"
+# Mostrar slices
+print(mg[-22:-24:-1] + " " + mg[9:17])    # DEVUELE LA LICENCIA
+
+# EJERCICIO 12
+mg="RUMBO AL LICENCIAMIENTO UNPRG"
+# Mostrar slices
+print(mg[24:])    # DEVUELE UNPRG
+
+# EJERCICIO 13
+mg="RUMBO AL LICENCIAMIENTO UNPRG"
+# Mostrar slices
+print(mg[0:])    # DEVUELE la cadena completa
+
+# EJERCICIO 14
+ms="NATHUS VINCERE"
+# Mostrar slices
+print(ms[0:7])  # DEVUELVE NATHUS
+
+# EJERCICIO 15
+ms="NATHUS VINCERE"
+# Mostra slices
+print(ms[-1:-4:-1] + "" + ms[5:6]) # Devuelve ERES
