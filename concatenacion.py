dfe771927b3ece951cb120750bc7addd119cdc86
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-12-13 02:22:02 -0500

    se desarrollo los ejercicicos de concatenacion

---
+++ concatenacion.py
@@ -0,0 +1,70 @@
+# concatenacion
+
+# EJERCICIO 1
+nombre="BRAYTON BRAVO"
+print("Mi nomnre es"+" "+nombre)            # se concatena: Mi nombre es BRAYTON BRAVO
+
+# EJERCICIO 2
+text="eres la luz de mis ojos"           # se concatena: Amada mia eres la luz de mis ojos
+print("Amada mia"+" "+text)
+
+# EJERCICIO 3
+mes1="¿como te encuentras?"
+msg2="\n"+"muy bien gracias a DIOS"      # se concatena: ¿como te encuentras?
+print(mes1+msg2)                                #         muy bien gracias a DIOS
+
+# EJERCICIO 4
+dni="73569819"                                    # se concatena: Mi numero de dni es 73569819
+print("Mi numero de DNI es" + " " + dni)
+
+# EJERCICIO 5
+cad="tome asiento por favor"                              # se concatena: tome asiento por favor
+print(cad+" "+"\n"+"si, gracias")                 #               si, gracias
+
+# EJERCICIO 6
+m="# Buenos dias vecina"                                      # se concatena: # Buenos dias vecina
+n="# buen dia vecino, ¿como amanecio?"                          #               # buen dia vecino, ¿como amanecio?
+m1="# un poco enfermo"                               #               # un poco enfermo
+print(m+"\n"+n+"\n"+m1)
+
+# EJERCICIO 7
+
+print("desde la primera ves que te vi"+" "+"me flechaste con tu mirada")# se concatena :desde la primera vez que te vi me flechaste con tu mirada
+
+# EJERCICIO 8
+mg="Somos una gran familia"
+print("Todos"+" "+mg)                           # se concatena: Todos somos una gran familia
+
+# EJERCICIO 9
+cad2="vuelve palomita vuelve"                                # se concatena: vuelve palomita vuelve
+cad3="vuelve a tu dulce nido"                                   #            vuelve a tu dulce nido
+print(cad2+"\n"+cad3)
+
+# EJERCICIO 10
+lote="¿aceptas ser mi esposa para toda la vida?"
+baba="\n" +"si y mi mil veces acepto"                                         # se concatena: ¿aceptas ser mi esposa para toda la vida?
+print(lote+baba)                                                            #              si y mil veces acepto
+
+# EJERCICIO 11
+print("mi curso favorito"+ "PROGRAMACION I")            # se concatena: mi curso favorito PROGRAMACION I
+
+# EJERCICIO 12
+ejm=":3"
+tes=":("
+print(ejm+"\r"+tes*4)                             # se concatena: :(:(:(:(
+
+# ejemplo 13
+md="El Amazonas"                              # se conatena: El Amazonas, Pulmon del mundo
+cmd=", 'Pulmon del mundo'"
+print(md+cmd)
+
+# EJERCICIO 14
+pr="estoy de orgulloso de las cosas que logrado"          # se concatena: estoy orgulloso de las cosas que logrado ¿y tu?
+lg="¿y tu?"                                               #               yo estoy orgullosa de verte triunfar
+cta="yo estoy orgulossa de verte\'triunfar\'"
+print(pr+" "+lg+"\n"+cta)
+
+# EJERCICIO 15
+msn="MERCEDES Y ROSALIA"                   # se concatena: Mis abuelos son MECERDES Y ROSALIA
+print("Mis abuelos son" + " " + msn)
+
