d01b2981354454296d66694b60444d26d2f3470b
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-12-13 02:21:37 -0500

    se desarrollo los ejercicicos de comparacion

---
+++ comparacion.py
@@ -0,0 +1,78 @@
+# COMPARACION
+
+# EJERCICIO 1
+msg1="hello"
+text1="hola"
+print(msg1== text1)
+
+# EJERCICIO 2
+msg2=" como te encuentras?"
+text2="bien gracias a Dios"
+print(msg2 != text2)
+
+# EJERCICIO 3
+nota="20"
+if (nota>="20" or nota=="10"):
+    print("aprovaste")
+
+# EJERCICIO 4
+msg3="mañana hay clases"
+text3="mañana si hay clases"
+print(msg3==text3)
+
+# EJERCICIO 5
+dni="73569819"
+celular="995733004"
+print(dni >= celular)
+
+# EJERCICIO 6
+msg5="PERU CAMPEON"
+tex5="peru campeom"
+print(msg5!=tex5)
+
+# EJERCICIO 7
+msg6="soy fiel a solo un equipo"
+text6="todo por una buena causa"
+print(msg6<text6)
+
+# EJERCICIO 8
+codigo="195692E"
+codigo1="1572668E"
+print(codigo == codigo1)
+
+# EJERCICIO 9
+nro_orden="30"
+calificion="15"
+print(nro_orden <= msg1)
+
+# EJRCICIO 10
+msg8="fea"
+text8="hermosa"
+print(msg8==text8)
+
+# EJERCICIO 11
+msg9="FELIZ DIA INTERNACIONAL DE LOS FIELES"
+text9="HOMBRES SIEMPRES FIELES"
+if (msg9 != text9):
+    print("FALSOS ")
+
+# EJERCICIO 12
+nombre="BRAYTON"
+print(nombre != "BRAYTON")
+
+# EJERCICIO 13
+frase="ELLA ES LA CHICA DE MIS SUEÑOS"
+if (frase!="chica de mis sueños"):
+    print("HERMOSA FRASE")
+
+# EJERCICIO 14
+fecha="07-01-02"
+mes="ENERO"
+if (fecha!=mes):
+    print("cumpleaños")
+
+# EJERCICIO 15
+msg14="alcanzo vacante"
+text14="no alcanzo vacante"
+if(msg14==text14):
+    print("no puede ingresar al sistema")
