dcfc86aa73b32d5a3794c7cc2b17e851fd651efc
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-12-13 02:23:33 -0500

    se desarrollo los ejercicicos de longitud

---
+++ longitud.py
@@ -0,0 +1,74 @@
+# EJERCICIO 1
+msg="FELIZ NAVIDAD"
+longitud=len(msg)                              # longitud =  13
+print("La longitud del msg:", longitud)
+
+# EJERCICIO 2
+msg2="\"A dormir mañana sera otro dia\'"
+long_1=len(msg2)                             # longitud = 31
+print("La longitud del msg2 es:", long_1)
+
+# EJERCICIO 3
+msg3="EL numero perfecto es : \"10\""       # longitud = 28
+long_2=len(msg3)
+print("La longitud del msg3 es:",long_2)
+
+# EJERCICIO 4
+msg4='¡yo quiero que me vean y digan si el puede yo puedo'                    # longitud = 51
+long_3= len(msg4)
+print("La longitud del msg4 es:",long_3)
+
+# EJERCICIO 5
+msg5=" ES TAN NATURAL QUERER QUERERNOS "           # longitud = 33
+long_4= len(msg5)
+print("La longitud del msg5 es:",long_4)
+
+# EJERCICIO 6
+cad_1="'DICEN QUE TU Y YO ESTAMOS LOCOS'"                 # longitud = 33
+long_5= len(cad_1)
+print("la longitud de la cad_1 es:",long_5)
+
+# EJERCICIO 7
+cad_2="\'SI ESTO NO ES AMOR SOY UN LOCO SIN MOTIVO\'"  # lomgitud = 43
+long_6= len(cad_2)
+print("La longitud de la cad_2 es:",long_6)
+
+# EJERCICIO 8
+cad_3="cuando mas grande es la dificultad, mas gloria hay en superarla"                # longitud = 63
+long_7= len(cad_3)
+print("La longitud de la cad_3 es:", long_7)
+
+# EJERCICIO 9
+cad_4='"Agua que no hay de beber dejala correr"'                 # longitud = 40
+long_8= len(cad_4)
+print("La longitud de la cad_4 es:", long_8)
+
+# EJERCICIO 10
+cad_5="\'¿por que nadie me quiere?\'"       # longitud = 27
+long_9= len(cad_5)
+print("la longitud de la cad_5 es:",long_9)
+
+# EJERCICIO 11
+nombre=" Brayton Bravo Araujo "              # longitud = 22
+long_10= len(nombre)
+print("la longitud del nombre es:",long_10)
+
+# EJERCICIO 12
+txt_1='PERRO QUE LADRA NO MUERDE'          # longitud = 25
+long_11= len(txt_1)
+print("La logitud del tex_1 es:", long_11)
+
+# EJERCICIO 13
+txt_2="'\'"
+long_12= len(txt_2)                                    # longitud = 2
+print("la longitud del text_2 es:",long_12)
+
+# EJERCICIO 14
+txt_3="LA ELECTRONICA ES MI PASION"
+long_13= len(txt_3)                                     # longitud = 27
+print("La longitud del tex_3 es:",long_13)
+
+# EJERCICIO 15
+txt_4="Mi curso favorito: \"PROGRAMACION\""
+long_14= len(txt_4)                                     # longitud = 33
+print("La longitud del text_4 es:", long_14)
