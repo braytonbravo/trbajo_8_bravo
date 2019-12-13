3745e06bab9f6040dea71857c8cd7000709a8168
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-12-13 02:21:06 -0500

    se desarrollo los ejercicicos de busquedad

---
+++ busquedad.py
@@ -0,0 +1,102 @@
+# BUSQUEDAD
+
+# EJERCICIO 1
+#              1         2
+#    012345678901234567890123456789012345
+msg1="CHONGOYAPE TIERRA DEL SOL Y ALEGRIA"
+print(msg1.find("CHONGOYAPE"))
+print(msg1.index("TIERRA"))
+print(msg1.index("SOL"))
+
+# EJERCICIO 2
+#     012345678901234567890123
+msg2="'RUMBO AL LINCENCIAMIENTO'"
+print(msg2.index("RUM"))
+print(msg2.find("MIENTO"))
+print(msg2.find("T"))
+
+# EJERCICIO 3
+#     012345678901234
+msg3="Peru es un mendigo sentado en una silla de oro"
+print(msg3.index("digo"))
+print(msg3.find("s"))
+print(msg3.index("or"))
+
+# EJERCICIO 4
+#     01234567890123456789
+msg4='"perro que ladra no muerde"'
+print(msg4.index("muerde"))
+print(msg4.find("a"))
+print(msg4.find("que"))
+
+# EJERCICIO 5
+#     01234567890123456789
+msg5='solo se que nada se'
+print(msg5.index('o'))
+print(msg5.find('nada'))
+print(msg5.index('solo'))
+
+# EJERCICIO 6
+#     01234567890123456789
+cad1="\'Messi el mejor jugador del mundo\'"
+print(cad1.index("mundo"))
+print(cad1.find("r"))
+print(cad1.find("Messi"))
+
+# EJERCICIO 7
+#     01234567890123456789
+cad2="EL AMAZONAS ES EL PULMON DEL MUNDO"
+print(cad2.index("PULMON"))
+print(cad2.find("ZONAS"))
+print(cad2.index("MUNDO"))
+
+# EJERCICIO 8
+#     01234567890123456789
+cad3='"camaron que se duerme se lo lleva la corriente"'
+print(cad3.index("se"))
+print(cad3.find("lo"))
+print(cad3.find("duerme"))
+
+# EJERCICIO 9
+#     01234567890123456789
+cad4='LOS HERALDOS NEGROS'
+print(cad4.index('RAL'))
+print(cad4.find('NEGRO'))
+print(cad4.index("ERA"))
+
+# EJERCICIO 10
+#     01234567890123456789
+cad5="NO DEJES PARA MAÑANA LO QUE PUEDES HACER HOY"
+print(cad5.index("MAÑANA"))
+print(cad5.find("EJES"))
+print(cad5.find("HOY"))
+
+# EJERCICIO 11
+#     01234567890123456789
+text1="\'MIGUEL GRAU EL CABALLERO DE LOS MARES\'"
+print(text1.index("GRAU"))
+print(text1.find("CABALLERO"))
+
+# EJERCICICIO 12
+#     01234567890123456789
+text2="Año de la lucha contra la corrupcion y la impuidad"
+print(text2.index("corrupcion"))
+print(text2.find("lucha"))
+
+# EJERCICIO 13
+#     01234567890123456789
+text3="'EL REY DE LA PROGRAMACION'"
+print(text3.index("REY"))
+print(text3.find("PROGRA"))
+
+# EJERCICIO 14
+#     01234567890123456789
+text4='ERES LO MEJOR Y LO PEOR QUE ME HA PASADO'
+print(text4.index('PEOR'))
+print(text4.find('MEJOR'))
+
+# EJERCICIO 15
+#     01234567890123456789
+text5="\'tierra de los bizcochuelos\'"
+print(text5.index("bizcochuelos"))
+print(text5.find("de"))
