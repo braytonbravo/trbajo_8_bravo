7614cbddf7bb5bd145a53ad55342dbff204ddc6d
braytonbravo <57871069+braytonbravo@users.noreply.github.com>
2019-12-13 02:23:12 -0500

    se desarrollo los ejercicicos de iteracion

---
+++ iteracion.py
@@ -0,0 +1,76 @@
+# iteracion
+
+# EJERCICIO 1
+cad1="BRAYTON ALDAHIR"
+for text in cad1:
+    print(text)
+
+# EJERCICIO 2
+cad2="SANGRE DE CAMPEON"
+for letra in cad2:
+    print(letra)
+
+# EJERCICIO 3
+codigo="195692E"
+for letra2 in codigo:
+    print(letra2)
+
+# EJERCICIO 4
+cad3="\'rojo es el color de mi corazon\'"
+for letra3 in cad3:
+    print(letra3)
+
+# EJERCICIO 5
+cad4="Año de la lucha contra la corrupcion y la impunidad"
+for letra4 in cad4:
+    print(letra4)
+
+# EJERCICIO 6
+it1='Lo pasado pasado no me interesa'
+for letra5 in it1:
+    print(letra5)
+
+# EJERCICIO 7
+it2="tengo un sentimiento muy adentro, un sentimiento que no tiene explicacion"
+for text2 in it2:
+    print(text2)
+
+# EJERCICIO 8
+it3="hay golpes en la vida tan fuerte yo no se"
+for text3 in it3:
+    print(text3)
+
+# EJERCICIO 9
+it4="La vida es el mejor regalo que Dios nos ha dado"
+for text4 in it4:
+    print(text4)
+
+# EJERCICIO 10
+it5="Todos tenemos un historia que contar"
+for text5 in it5:
+    print(text5)
+
+# EJERCICIO 11
+msn="CACHIMBOS 2019 II"
+for itera in msn:
+    print(itera)
+
+# EJERCICIO 12
+msn2="Eres la luz que me ilumnina desde el cielo"
+for itera2 in msn2:
+    print(itera2)
+
+# EJERCICIO 13
+msn3="Guerra avisada no mata gente"
+for itera3 in msn3:
+    print(itera3)
+
+# EJERCICIO 14
+msn4="EL QUE MADRUGA DIOS AYUDA"
+for itera4 in msn4:
+    print(itera4)
+
+# EJERCICIO 15
+msn5="CUANDO EL RIO SUENA ES PORQUE PIEDRA TRAE"
+for itera5 in msn5:
+    print(itera5)
